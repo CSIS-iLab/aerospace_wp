18e01382f4b416c60696b987ebb16a57ce1c7312
ilab_intern <ilab_intern@ilabintern2s-iMac.local>
2017-08-23 16:48:24 -0400

    Add sources custom meta to aerospace101 cpt

--- wp-content/themes/aerospace/inc/custom-post-types.php
+++ wp-content/themes/aerospace/inc/custom-post-types.php
@@ -9,8 +9,6 @@
 require get_template_directory() . '/inc/cpts/aerospace101.php';
 /*----------  Data  ----------*/
 require get_template_directory() . '/inc/cpts/data.php';
-
-
 /*----------  Events  ----------*/
 /*
 require get_template_directory() . '/inc/cpts/cpt-events.php';
