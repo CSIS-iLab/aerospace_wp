f72a564ea01acfe9904ed914ee01f22467431aac
Jacque Schrag <jnschrag@gmail.com>
2017-11-20 18:42:09 -0500

    Show header navigation on hover.

--- wp-content/themes/aerospace/inc/aerospace-header-menu-walker.php
+++ wp-content/themes/aerospace/inc/aerospace-header-menu-walker.php
@@ -8,16 +8,6 @@
 class Aerospace_Menu extends Walker_Nav_Menu {
 
     function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<div class='sub-menu-container row'>
-            <div class='col-xs-12 col-md-3 submenu-parent'>
-            " . $args->parent_title . "
-            </div>\n
-            <div class='col-xs-12 col-md-3 submenu-children'>\n
-                <ul class='sub-menu'>\n";
-    }
-
-    function end_lvl( &$output, $depth = 0, $args = array() ) {
         global $menu_featured_post, $menu_featured_post_description;
 
         if ( isset ( $menu_featured_post ) ) {
@@ -31,14 +21,22 @@ class Aerospace_Menu extends Walker_Nav_Menu {
         }
 
         $indent = str_repeat("\t", $depth);
-        $output .= "$indent</ul>\n
+        $output .= "\n$indent<div class='submenu-container row'>
+            <div class='col-xs-12 col-md-3 submenu-parent'>
+            " . $args->parent_title . "
             </div>\n
             <div class='col-xs-12 col-md-6 submenu-featured-post'>
                 <p class='submenu-featured-post-desc'>" . $menu_featured_post_description . "</p>
                 " . $featured_post_html . "
             </div>\n
-        </div>\n
-        </div>\n";
+            <div class='col-xs-12 col-md-3 submenu-children'>\n
+                <ul class='sub-menu'>\n";
+    }
+
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n
+            </div>\n";
     }
 
     function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
@@ -63,7 +61,7 @@ class Aerospace_Menu extends Walker_Nav_Menu {
         $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
         $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
         $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
-        $output .= $indent . '';
+        $output .= $indent . '<li>';
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
         $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
@@ -72,17 +70,17 @@ class Aerospace_Menu extends Walker_Nav_Menu {
         $item_output .= '<a'. $attributes .'>';
         $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
         $item_output .= '</a>';
-        $item_output .= $args->after;
-
         if ( $depth == 1 ) {
             $item_output .= '<p class="submenu-desc">' . $item->description . '</p>';
         }
+        $item_output .= '';
+        $item_output .= $args->after;
 
         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
 
     function end_el( &$output, $item, $depth = 0, $args = array() ) {
-        $output .= "\n";
+        $output .= "</li>\n";
     }
 }
 
