839e00ce3b5766ee4afea7c2b716a709454a58e2
ilab_intern <ilab_intern@ilabintern2s-iMac.local>
2017-11-21 15:10:08 -0500

    Add view shortcode

--- wp-content/themes/aerospace/inc/shortcodes.php
+++ wp-content/themes/aerospace/inc/shortcodes.php
@@ -16,4 +16,24 @@ function shortcode_first( $atts , $content = null ) {
 	return "<span class='post-first-sentence'>".do_shortcode($content)."</span>";
 }
 add_shortcode( 'first', 'shortcode_first' );
+
+/**
+ * Adds inline social sharing component to podcasts, stats, and interactives embedded in posts via shortcode
+ * @param  string $title Title to be used by social media
+ * @param  string $URL   URL to be used by social media
+ * @return string        HTML of share button
+ */
+ function shortcode_view( $atts ) {
+	 $atts = shortcode_atts(
+		 	array(
+			 	'id' => null,
+		 	),
+			$atts,
+			'view'
+	 );
+	 $post_title = get_the_title($atts['id']);
+	 $post_url = get_the_permalink($atts['id']);
+	 return "<div class='view-post'><a href='" . esc_url( $post_url ) . "'>" . esc_attr( $post_title ) . "</a></div>";
+}
+add_shortcode( 'view', 'shortcode_view' );
  ?>
