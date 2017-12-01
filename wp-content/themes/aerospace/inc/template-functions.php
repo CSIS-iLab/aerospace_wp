94885ef21b9ed30449ac5f0f7873ee4f42106bac
Jacque Schrag <jnschrag@gmail.com>
2017-12-01 13:51:35 -0500

    Insert images with relative URLs

--- wp-content/themes/aerospace/inc/template-functions.php
+++ wp-content/themes/aerospace/inc/template-functions.php
@@ -142,3 +142,16 @@ function aerospace_the_content_shortcode_fix($content) {
     return strtr($content, $array);
 }
 add_filter('the_content', 'aerospace_the_content_shortcode_fix');
+
+/**
+ * Use relative URLs for images
+ */
+function aerospace_switch_to_relative_url($html, $id, $caption, $title, $align, $url, $size, $alt)
+{
+    $imageurl = wp_get_attachment_image_src($id, $size);
+    $relativeurl = wp_make_link_relative($imageurl[0]);   
+    $html = str_replace($imageurl[0],$relativeurl,$html);
+          
+    return $html;
+}
+add_filter('image_send_to_editor','aerospace_switch_to_relative_url',10,8);
