a466f6b59f4bbc1ebce904dbb5ad90c0e93c8091
Jacque Schrag <jnschrag@gmail.com>
2017-12-01 12:09:48 -0500

    Add default page styles and specific styles for the about page

--- wp-content/themes/aerospace/functions.php
+++ wp-content/themes/aerospace/functions.php
@@ -95,19 +95,6 @@ endif;
 add_action('after_setup_theme', 'aerospace_setup');
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function aerospace_content_width()
-{
-    $GLOBALS['content_width'] = apply_filters('aerospace_content_width', 640);
-}
-add_action('after_setup_theme', 'aerospace_content_width', 0);
-
-/**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
