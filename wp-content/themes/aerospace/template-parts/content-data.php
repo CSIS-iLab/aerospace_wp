0d7752aa9fe657fbc9c3e9d0154a3f35ff8c0ae2
Jacque Schrag <jnschrag@gmail.com>
2018-03-02 12:34:17 -0500

    Hide event subheadings on archive pages and add missing if/else statement for data repo content.

--- wp-content/themes/aerospace/template-parts/content-data.php
+++ wp-content/themes/aerospace/template-parts/content-data.php
@@ -12,29 +12,7 @@ $classes = 'row card-format';
 $thumbnail_size = 3;
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-	<?php
-	if ( has_post_thumbnail() ) {
-		echo '<div class="col-xs-12 col-md-' . $thumbnail_size . ' post-thumbnail"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
-		the_post_thumbnail( 'medium_large' );
-		echo '</a></div>';
-	}
-	?>
-	<div class="col-xs-12 col-md entry-main">
-        <header class="entry-header">
-            <?php
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-            ?>
-            <div class="entry-meta">
-                <?php aerospace_last_updated(); ?>
-                <?php aerospace_entry_categories(); ?>
-                <?php aerospace_entry_tags(); ?>
-            </div><!-- .entry-meta -->
-        </header><!-- .entry-header -->
-    </div>
-</article>
-
-
+<?php if ( is_post_type_archive( 'data' ) ) { ?>
 <tr id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
 	<td class="display-column row" colspan="5">
 		<?php
@@ -62,3 +40,27 @@ $thumbnail_size = 3;
 	<td><?php aerospace_entry_data_categories(); ?></td>
 	<td><?php aerospace_entry_tags(); ?></td>
 </tr>
+<?php } else { ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
+	<?php
+	if ( has_post_thumbnail() ) {
+		echo '<div class="col-xs-12 col-md-' . $thumbnail_size . ' post-thumbnail"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+		the_post_thumbnail( 'medium_large' );
+		echo '</a></div>';
+	}
+	?>
+	<div class="col-xs-12 col-md entry-main">
+        <header class="entry-header">
+            <?php
+            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+            ?>
+            <div class="entry-meta">
+                <?php aerospace_last_updated(); ?>
+                <?php aerospace_entry_categories(); ?>
+                <?php aerospace_entry_tags(); ?>
+            </div><!-- .entry-meta -->
+        </header><!-- .entry-header -->
+    </div>
+</article>
+<?php }
